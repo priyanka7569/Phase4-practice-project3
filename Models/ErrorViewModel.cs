98674c05b6e90af6a1c8e4e35b55495f2e657589
priyanka7569 <kota.priyanka@mphasis.com>
2024-01-19 12:43:06 +0530

    Practise project 3 modified

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppBankLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
